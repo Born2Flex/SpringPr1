62c2da4a184d92a9f58102291b148d3b1fb1e398
Danylo Shlapak <geografy2314@gmail.com>
2023-09-08 08:41:45 +0300

    init

---
+++ src/test/java/com/ukma/firstpractice/FirstPracticeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ukma.firstpractice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
